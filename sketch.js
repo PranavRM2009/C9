306d8062754bdada0c23b44fc166eb385315aac6
PranavRM2009 <106379264+PranavRM2009@users.noreply.github.com>
2022-05-27 18:29:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let box
+function setup() {
+  createCanvas(700, 700);
+  box=createSprite(500,500,50,50)
+}
+
+function draw() {
+  background(220);
+  
+  //make the box move using arrow keys
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x + 5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x - 5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y + 5
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y - 5
+  }
+  drawSprites()
+  
+}
+
+
+
+
